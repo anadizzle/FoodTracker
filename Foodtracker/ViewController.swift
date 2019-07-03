4458fecb9aaacd90c7ad890f39f020f75bc9920d
Anadi Misra <anadi213lbc@gmail.com>
2019-07-03 06:01:11 -0400

    Initial Commit

---
+++ Foodtracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Foodtracker
+//
+//  Created by Anadi Misra on 2019-07-03.
+//  Copyright © 2019 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
